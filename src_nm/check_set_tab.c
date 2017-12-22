14b48aadf99148188232e2b70bc240a5a3af0067
linaa2 <alexis.linaa2@mail.dcu.ie>
2017-12-22 12:31:40 +0000

    Add all src

---
+++ src_nm/check_set_tab.c
@@ -0,0 +1,129 @@
+/*
+** check_set_tab.c for check_set_tab in
+**
+** Made by lina_a
+** Login   <lina_a@epitech.net>
+**
+** Started on  Fri Feb 26 13:55:12 2016 lina_a
+** Last update Fri Feb 26 13:55:12 2016 lina_a
+*/
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <elf.h>
+#include <string.h>
+#include "objdump.h"
+#include "nm.h"
+
+int        check_truncated_file(char *name_file, int fd)
+{
+    if (verification(name_file) == -1)
+    {
+        dprintf(STDERR_FILENO, "/usr/bin/nm: %s: File truncated\n",
+                name_file);
+        close(fd);
+	return (-1);
+    }
+    return (0);
+}
+
+Elf64_Shdr	*set_sym_tab(Elf64_Shdr **string_sec, char *str,
+			     Elf64_Ehdr *elf,
+			     Elf64_Shdr *shdr)
+{
+    int			i;
+    Elf64_Shdr		*symsection;
+
+    symsection = NULL;
+    *string_sec = NULL;
+    i = 0;
+    while (i < elf->e_shnum)
+    {
+        if (shdr[i].sh_size)
+        {
+            if (!strcmp(&str[shdr[i].sh_name], ".symtab"))
+                symsection = &shdr[i];
+            if (!strcmp(&str[shdr[i].sh_name], ".strtab"))
+                *string_sec = &shdr[i];
+        }
+        i++;
+    }
+    return (symsection);
+}
+
+void		which_header_correct_is(void *data, char *name_file, int fd)
+{
+    Elf64_Ehdr *elf;
+    Elf32_Ehdr *elf32;
+
+    if (check_header((Elf64_Ehdr *) data) != -1)
+    {
+      if ((check_truncated_file(name_file, fd)) == -1)
+	return;
+        elf = (Elf64_Ehdr *) data;
+        if (elf->e_ident[EI_CLASS] == ELFCLASS64)
+            my_nm(data, elf);
+    }
+    else if (check_header_elf_32(((Elf32_Ehdr *) data)) != -1)
+    {
+      if ((check_truncated_file(name_file, fd)) == -1)
+	return;
+      elf32 = (Elf32_Ehdr *) data;
+      if (elf32->e_ident[EI_CLASS] == ELFCLASS32)
+	my_nm_32(data, elf32);
+    }
+    else
+    {
+        dprintf(STDERR_FILENO, "/usr/bin/nm: %s: File format not "
+                "recognized\n", name_file);
+        close(fd);
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void    check_file(int fd, char *av)
+{
+    struct stat file;
+    void *data;
+
+    if ((fstat(fd, &file)) == -1)
+        return;
+    if ((data = mmap(NULL, (size_t) file.st_size, PROT_READ,
+                     MAP_SHARED | MAP_FILE, fd, 0)) == MAP_FAILED)
+    {
+        dprintf(STDERR_FILENO, "%m\n");
+        return;
+    }
+    which_header_correct_is(data, av, fd);
+    if ((munmap(data, (size_t) file.st_size)) == -1)
+    {
+        dprintf(STDERR_FILENO, "%m\n");
+        close(fd);
+        return;
+    }
+}
+
+char        *my_nm_32(void *data, Elf32_Ehdr *elf)
+{
+    unsigned int             idx;
+    Elf32_Shdr		*shdr;
+    Elf32_Sym       sym;
+    Elf32_Shdr		*pShdr;
+    char            *str;
+
+    idx = 0;
+    shdr = (Elf32_Shdr*)(data + elf->e_shoff);
+    str = (char*)(data + shdr[elf->e_shstrndx].sh_offset);
+    if ((pShdr = set_sym_tab32bits(&shdr, str, elf, shdr)) == NULL)
+        return (NULL);
+    while (idx < (pShdr->sh_size / pShdr->sh_entsize))
+    {
+        sym = ((Elf32_Sym*)(data + pShdr->sh_offset))[idx];
+        print_sec_and_sym32bits(sym, data, shdr);
+        idx++;
+    }
+    return (0);
+}
---
+++ src_nm/main.c
@@ -0,0 +1,129 @@
+/*
+** main.c for main in /home/lina_a/rendu/B4/PSU_2016_nmobjdump/src_nm
+**
+** Made by lina_a
+** Login   <lina_a@epitech.net>
+**
+** Started on  Sun Feb 21 20:06:46 2016 lina_a
+** Last update Sun Feb 21 20:06:46 2016 lina_a
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <elf.h>
+#include "nm.h"
+
+char                print_char_elf2(Elf64_Sym sym, Elf64_Shdr *shdr)
+{
+    char            c;
+
+    c = '\0';
+    if (sym.st_shndx == SHN_ABS)
+      c = 'A';
+    else if (sym.st_shndx == SHN_COMMON)
+      c = 'C';
+    else if (ELF64_ST_TYPE(sym.st_info) == STT_OBJECT
+	     && shdr[sym.st_shndx].sh_flags == 3)
+      c = 'B';
+    else if (ELF64_ST_TYPE(sym.st_info)== STT_OBJECT
+             && shdr[sym.st_shndx].sh_flags == 2)
+      c = 'R';
+    else if (ELF64_ST_TYPE(sym.st_info) == STT_OBJECT)
+      c = 'D';
+    else if (ELF64_ST_TYPE(sym.st_info) == STT_FUNC)
+      c = 'T';
+    if ((shdr[sym.st_shndx].sh_type == SHT_DYNAMIC &&
+	 ELF64_ST_TYPE(sym.st_info) == STT_OBJECT))
+      c = 'D';
+    else if (sym.st_shndx == SHN_UNDEF)
+        c = 'U';
+    return c;
+}
+
+char                print_char_elf(Elf64_Sym sym, Elf64_Shdr *shdr)
+{
+    char            c;
+
+    if (ELF64_ST_BIND(sym.st_info) == STB_GNU_UNIQUE)
+        c = 'u';
+    else if (ELF64_ST_BIND(sym.st_info) == STB_WEAK)
+    {
+        c ='W';
+        if (sym.st_shndx == SHN_UNDEF)
+            c = 'w';
+    }
+   else if (shdr[sym.st_shndx].sh_flags == 1)
+        c = 'B';
+    else if (ELF64_ST_BIND(sym.st_info) == STB_WEAK &&
+            ELF64_ST_BIND(sym.st_info) == STT_OBJECT)
+    {
+        c = 'V';
+        if (sym.st_shndx == SHN_UNDEF)
+            c = 'v';
+    }
+    else if ((c = print_char_elf2(sym, shdr)) != '\0');
+    else
+      c = '?';
+    if (ELF64_ST_BIND(sym.st_info) == STB_LOCAL && c != '?')
+        c += 32;
+    return c;
+}
+
+void                print_sec_and_sym(Elf64_Sym sym, void *data,
+				      Elf64_Shdr *shdr)
+{
+    if (sym.st_name != 0)
+    {
+        if (sym.st_value != 0 && sym.st_info != STT_FILE)
+            printf("%016x %c %s\n", (unsigned int) sym.st_value,
+                   print_char_elf(sym, shdr),
+                   (char *) (data + shdr->sh_offset + sym.st_name));
+        else if (sym.st_info != STT_FILE)
+            printf("%16s %c %s\n", " ", print_char_elf(sym, shdr),
+                   ((char *)(data + shdr->sh_offset + sym.st_name)));
+    }
+}
+
+char                *my_nm(void *data, Elf64_Ehdr *elf)
+{
+    unsigned int    idx;
+    Elf64_Shdr	    *shdr;
+    Elf64_Sym       sym;
+    Elf64_Shdr	    *pShdr;
+    char            *str;
+
+    idx = 0;
+    shdr = (Elf64_Shdr*)(data + elf->e_shoff);
+    str = (char*)(data + shdr[elf->e_shstrndx].sh_offset);
+    if ((pShdr = set_sym_tab(&shdr, str, elf, shdr)) == NULL)
+        return (NULL);
+    while (idx < (pShdr->sh_size / pShdr->sh_entsize))
+    {
+        sym = ((Elf64_Sym*)(data + pShdr->sh_offset))[idx];
+        print_sec_and_sym(sym, data, shdr);
+        idx++;
+    }
+  return (0);
+}
+
+int   main(int ac, char **av)
+{
+  int i;
+  int fd;
+
+  i = 0;
+  if (ac >= 2)
+  {
+    while (++i < ac)
+    {
+      if ((fd = open(av[i], O_RDONLY)) > 0)
+          check_file(fd, av[i]);
+      close(fd);
+    }
+  }
+  else
+      dprintf(STDERR_FILENO, "/usr/bin/nm 'a.out': No such file\n");
+  return (0);
+}
